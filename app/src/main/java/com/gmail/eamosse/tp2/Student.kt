a0dac7f32b960fa175bc269c8f9ad3ee1d1c839a
Ethan Carollo <etcarollo@gmail.com>
2024-11-12 14:27:20 +0100

    👷 work work work workeeuhhh

--- app/src/main/java/com/gmail/eamosse/tp2/Student.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/Student.kt
@@ -2,5 +2,9 @@ package com.gmail.eamosse.tp2
 
 //TODO : Complete the class
 data class Student {
-
+    var name: String
+    var code: String
+    var sexe: Char
+    var address: String
+    var age: Int
 }
